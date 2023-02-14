24962fc36a0b6ed6efcc098c6e3e31a9b1108ec4
AlexandreGuinaudeau <aguinaudeau@palantir.com>
2023-02-14 15:15:15 +0100

    Validate CRAN package on R-devel (#18)

---
+++ R/utils.R
@@ -0,0 +1,21 @@
+#  (c) Copyright 2023 Palantir Technologies Inc. All rights reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# R 4.2.0 does not consider code in classes created at install time, so the dependency check fails
+# Workaround per https://community.rstudio.com/t/new-r-cmd-check-note-in-r-4-2-0-for-imports-field/143153/2
+#' @keywords internal
+unused <- function() {
+  R6::R6Class
+  jsonlite::toJSON
+}
